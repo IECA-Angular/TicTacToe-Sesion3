c6bcc6806436826feb16a0b70dfd067230eb73f4
Josué Caro <josuecaro@MacBook-Pro-de-Josue.local>
2023-06-22 09:49:10 -0600

    Sesiones

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'TicTacToe'`, () => {
+  it(`should have as title 'tic-tac-toe'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('TicTacToe');
+    expect(app.title).toEqual('tic-tac-toe');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('TicTacToe app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('tic-tac-toe app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,149 @@
 import { Component } from '@angular/core';
 
+interface IWinner {
+  player: number;
+  date: Date;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  player: number = 1;
+  gameStatus!: 'play' | 'tie' | 'win';
+  board!: number[];
+  playerWinner!: number;
 
-  estadoJuego: 'jugando' | 'espera' | 'finalizado' = 'espera';
-  jugadorActual: number = 1;
-  ganador: number = 0;
-  celdas: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  /**
+   Scoreboard
+   */
+  winnersBoard: IWinner[] = [];
+  showScoreboard: boolean = false;
 
 
-  clickCelda(indice: number) {
+  constructor() {
+    this.newGame();
+    this.loadWinners();
+  }
 
-    /** Este if ponerlo despues para mostrar que pasa si no existe */
-    if (this.celdas[indice] !== 0) {
-      return;
+  newGame() {
+    this.player = 1;
+    this.gameStatus = 'play';
+    this.playerWinner = 0;
+    /*
+     0 -> No hay celda seleccionada
+     1 -> Jugador 1 seleccionó la celda
+     2 -> Jugador 2 seleccionó la celda
+     */
+    this.board = [0, 0, 0,
+                  0, 0, 0,
+                  0, 0, 0];
+  }
+
+  selectCell(index: number) {
+
+      if (this.gameStatus !== 'play') {
+        return;
+      }
+
+      if (this.board[index] !== 0) {
+        return;
+      }
+
+
+    this.board[index] = this.player;
+    this.player = this.player === 1 ? 2 : 1;
+    /**
+     if (this.player === 1) {
+      this.player = 2;
+     } else {
+      this.player = 1;
+     }
+     */
+
+     this.checkGameStatus();
+
+  }
+
+
+  checkGameStatus() {
+    // Verificar si hay un ganador en alguna fila
+    for (let i = 0; i < 9; i += 3) {
+      if (
+        this.board[i] !== 0 &&
+        this.board[i] === this.board[i + 1] &&
+        this.board[i + 1] === this.board[i + 2]
+      ) {
+        this.setWinner(this.board[i]);
+        return;
+      }
     }
 
-     this.celdas[indice] = this.jugadorActual;
-     this.cambiarJugador();
+    // Verificar si hay un ganador en alguna columna
+    for (let i = 0; i < 3; i++) {
+      if(
+        this.board[i] !== 0 &&
+        this.board[i] === this.board[i+3] &&
+        this.board[i+3] === this.board[i+6]
+      ) {
+        this.setWinner(this.board[i]);
+        return;
+      }
+    }
+
+    // Verificar primera diagonal
+    if (this.board[0] !== 0 &&
+      this.board[0] === this.board[4] &&
+      this.board[4] === this.board[8]
+      ) {
+        this.setWinner(this.board[0]);
+        return;
+      }
+
+    // Verificar segunda diagonal
+    if (this.board[2] !== 0 &&
+      this.board[2] === this.board[4] &&
+      this.board[4] === this.board[6]
+      ) {
+        this.setWinner(this.board[2]);
+        return;
+      }
+
+     // Verificar si hay empate
+     if(this.board.indexOf(0) === -1) {
+      this.setTie();
+     }
+
   }
 
-  cambiarJugador() {
-    this.jugadorActual = this.jugadorActual === 1 ? 2 : 1;
+
+  setWinner(player: number) {
+    this.gameStatus = 'win';
+    this.playerWinner = player;
+    this.saveWinner();
   }
+
+  setTie() {
+    this.gameStatus = 'tie';
+  }
+
+  saveWinner() {
+    const winner: IWinner = {
+      player: this.playerWinner,
+      date: new Date()
+    };
+    this.winnersBoard.push(winner);
+    localStorage.setItem('winners', JSON.stringify(this.winnersBoard));
+  }
+
+  loadWinners() {
+    const winners = localStorage.getItem('winners');
+    if (winners) {
+      this.winnersBoard = JSON.parse(winners);
+    }
+  }
+
 
 }
