203a9404b64e10ea7f5ac5ba1aab0277eec402f2
Josué Caro <josuecaro@MacBook-Pro-de-Josue.local>
2023-06-22 09:46:59 -0600

    Tic tac toe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'TicTacToe';
+
+  estadoJuego: 'jugando' | 'espera' | 'finalizado' = 'espera';
+  jugadorActual: number = 1;
+  ganador: number = 0;
+  celdas: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+
+  clickCelda(indice: number) {
+
+    /** Este if ponerlo despues para mostrar que pasa si no existe */
+    if (this.celdas[indice] !== 0) {
+      return;
+    }
+
+     this.celdas[indice] = this.jugadorActual;
+     this.cambiarJugador();
+  }
+
+  cambiarJugador() {
+    this.jugadorActual = this.jugadorActual === 1 ? 2 : 1;
+  }
+
 }
